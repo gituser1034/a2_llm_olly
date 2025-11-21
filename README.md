3f76715253dbebac8eaebee13c76e09dc33dad32
gituser1034 <olly.love@ontariotechu.net>
2025-11-21 17:07:48 -0500

    added code

--- README.md
+++ README.md
@@ -3,9 +3,10 @@ Olly Love
 Ollama + Python LLM Study Buddy
 
 How to run:
-Install Ollama on Windows + pull model gemma3:4b, create venv (python -m venv venv)
+Install Ollama on Windows + pull model gemma3:4b (change to currently used model), create venv (python -m venv venv)
 Run venv: venv\Scripts\activate
 pip install ollama in active python venv
+pip install langchain langchain-ollama langchain-chroma
 python main.py
 
 
--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Hello World")
+# Below in venv
+# Im using local llm so may have to modify slightly
+from langchain_ollama.llms import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+
+model = OllamaLLM(model="qwen3:4b")
+
+# Placeholder for read in file later
+template = """
+You are an expert in answering questions about a pizza restaurant.
+
+Here are some relevant reviews: {reviews}
+
+Here is the question to answer: {question}
+"""
+
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
+
+result = chain.invoke({"Reviews": [], "question": "What is the best pizza place in town?"})
+print(result)

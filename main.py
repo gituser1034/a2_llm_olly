a44410180df4dd4476198337948254d5a5395548
gituser1034 <olly.love@ontariotechu.net>
2025-12-02 16:21:14 -0500

    added telemetry

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ from langchain_core.prompts import ChatPromptTemplate
 from vectorpdf import retriever
 from pydantic import BaseModel
 from typing import List
+import datetime
+import time
 
 system_prompt_path = 'prompts/prompt.txt'
+telemetry = 'telemetry.txt'
 
 # How to test
 # Offline testing - Im not testing the whole flow, just the grading part
@@ -48,36 +51,58 @@ results = []
 questions = []
 models_answers = []
 
-# Retrieves notes to use in generation
-astro_notes = retriever.invoke("Ask astronomy questions and provide answers")
-output = chain.invoke({"astro_notes": astro_notes})
-results = output.split("\n")
+with open(telemetry, 'a', encoding='utf-8') as file:
+  # Retrieves notes to use in generation through RAG
+  file.write(str(datetime.datetime.now()) + ": RAG retrieval underway - accessing AstroNotes.pdf chroma_langchain_db vector store\n")
+  start_time = time.time()
+  astro_notes = retriever.invoke("Ask astronomy questions and provide answers")
+  output = chain.invoke({"astro_notes": astro_notes})
+  end_time = time.time()
+  file.write(str(datetime.datetime.now()) + ": RAG data retrieval successful\n")
+  file.write("Model:\n" + output + "\n")
+  file.write("RAG Latency: " + str(end_time - start_time)  + "\n")
 
-# accessing first index and checking whether its Q or A
-# to seperately store questions and answers
-for i in range(len(results)):
-  line = results[i].strip()
-  if not line:
-    continue
-  # Checking first char in the line
-  if line[0] == "Q":
-    questions.append(line)
-  elif line[0] == "A":
-    models_answers.append(line + "\n")
-  else:
-    continue
+  file.write(str(datetime.datetime.now()) + ": Caching data.\n")
+  start_time2 = time.time()
+  results = output.split("\n")
 
-for i in range(len(questions)):
-  print(questions[i])
-  print("---------------------------")
+  # accessing first index and checking whether its Q or A
+  # to seperately store questions and answers
+  for i in range(len(results)):
+    line = results[i].strip()
+    if not line:
+      continue
+    # Checking first char in the line
+    if line[0] == "Q":
+      questions.append(line)
+    elif line[0] == "A":
+      models_answers.append(line + "\n")
+    else:
+      continue
 
-  student_answer = input("Answer the question (q to quit): ")
+  file.write(str(datetime.datetime.now()) + ": Data cached.\n")
+  end_time2 = time.time()
+  file.write("Caching Latency: " + str(end_time2 - start_time2) + "\n")
 
-  if student_answer == "q":
-    break
+  for i in range(len(questions)):
+    print(questions[i])
+    print("---------------------------")
+    file.write(str(datetime.datetime.now()) + ": Cached question output: "  + questions[i] + "\n")
 
-  # Passing context the answer prompt expects
-  output = chain2.invoke({"models_answer": models_answers[i], "question": questions[i], "student_answer":student_answer})
-  print(output)
+    student_answer = input("Answer the question (q to quit): ")
+    file.write(str(datetime.datetime.now()) + ": Student: " + student_answer + "\n")
 
-  print("---------------------------")
+    if student_answer == "q":
+      break
+
+    file.write(str(datetime.datetime.now()) + ": Local cache retrieval underway.\n")
+    start_time3 = time.time()
+    # Passing context the answer prompt expects
+    output = chain2.invoke({"models_answer": models_answers[i], "question": questions[i], "student_answer":student_answer})
+    end_time3 = time.time()
+    file.write(str(datetime.datetime.now()) + ": Cached data retrieval successful\n")
+    file.write(str(datetime.datetime.now()) + ": Model: " + output  + "\n")
+    file.write("Cache retrieval latency: " + str(end_time3 - start_time3))
+    print(output)
+
+    print("---------------------------")

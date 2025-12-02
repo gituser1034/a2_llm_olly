c2323f9441a34f3238cfedf43db9daae38cf2bd5
gituser1034 <olly.love@ontariotechu.net>
2025-12-02 18:56:29 -0500

    finished telemetry and inputs

--- test_main.py
+++ test_main.py
@@ -1,3 +1,6 @@
+# Testing LLM Grading ability
+# Using questions generated in a previous round to test models grading ability
+
 # Below in venv
 from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
@@ -6,19 +9,36 @@ from pydantic import BaseModel
 from typing import List
 import datetime
 import time
+import json
 
-telemetry = 'samples/telemetry.txt'
+telemetry = 'samples/telemetry_test.txt'
 
 model = OllamaLLM(
   model="qwen3:4b",
 )
 
-question_prompt_path = 'prompts/q_prompt.txt'
+test_prompt_path = 'prompts/test_prompt.txt'
 answer_prompt_path = 'prompts/a_prompt.txt'
+inputs_path = 'prompts/inputs.json'
+inputs = []
+questions = []
+expected_answers = []
+# Tracks how many questions correct
+acc_count = 0
+model_acc_count = 0
+
+# Reading in inputs from json file
+with open(inputs_path, 'r', encoding='utf-8') as file:
+  data = json.load(file)
+
+for input_key in data:
+  inputs.append(data[input_key]["input"])
+  questions.append([input_key]["question"])
+  expected_answers.append([input_key]["expected"])
 
 # Getting question prompt
-with open(question_prompt_path, 'r', encoding='utf-8') as file:
-  question_prompt = file.read().strip()
+with open(test_prompt_path, 'r', encoding='utf-8') as file:
+  test_prompt = file.read().strip()
 
 # Getting prompt for students answer
 with open(answer_prompt_path, 'r', encoding='utf-8') as file:
@@ -26,7 +46,7 @@ with open(answer_prompt_path, 'r', encoding='utf-8') as file:
 
 # 1 chain using RAG for the model to generate a question
 # another chain for the model to respond to the user
-prompt = ChatPromptTemplate.from_template(question_prompt)
+prompt = ChatPromptTemplate.from_template(test_prompt)
 chain = prompt | model
 prompt2 = ChatPromptTemplate.from_template(answer_prompt)
 chain2 = prompt2 | model
@@ -36,61 +56,71 @@ print("Welcome to the Ollama LLM Astronomy True or False Quiz.")
 # Local caches - Initial RAG generation data stored here for
 # ideally faster chat 
 results = []
-questions = []
 models_answers = []
 
 with open(telemetry, 'a', encoding='utf-8') as file:
-  # Retrieves notes to use in generation through RAG
+  # From the models previously generated set of questions I have stored with my test cases
+  # Generate answers stored in a cache for later grading
   file.write(str(datetime.datetime.now()) + ": RAG retrieval underway - accessing AstroNotes.pdf chroma_langchain_db vector store\n")
   start_time = time.time()
-  astro_notes = retriever.invoke("Ask astronomy questions and provide answers")
-  output = chain.invoke({"astro_notes": astro_notes})
+  output = chain.invoke({"test_questions": questions})
   end_time = time.time()
   file.write(str(datetime.datetime.now()) + ": RAG data retrieval successful\n")
-  file.write("Model:\n" + output + "\n")
   file.write("RAG Latency: " + str(end_time - start_time)  + "\n")
 
   file.write(str(datetime.datetime.now()) + ": Caching data.\n")
   start_time2 = time.time()
   results = output.split("\n")
 
-  # accessing first index and checking whether its Q or A
-  # to seperately store questions and answers
+  # Storing model answers
   for i in range(len(results)):
     line = results[i].strip()
     if not line:
       continue
-    # Checking first char in the line
-    if line[0] == "Q":
-      questions.append(line)
-    elif line[0] == "A":
+    if line[0] == "A":
       models_answers.append(line + "\n")
-    else:
-      continue
 
   file.write(str(datetime.datetime.now()) + ": Data cached.\n")
   end_time2 = time.time()
   file.write("Caching Latency: " + str(end_time2 - start_time2) + "\n")
 
-  for i in range(len(questions)):
+  for i in range(len(inputs)):
     print(questions[i])
     print("---------------------------")
     file.write(str(datetime.datetime.now()) + ": Cached question output: "  + questions[i] + "\n")
 
-    student_answer = input("Answer the question (q to quit): ")
-    file.write(str(datetime.datetime.now()) + ": Student: " + student_answer + "\n")
+    file.write(str(datetime.datetime.now()) + ": Student: " + inputs[i] + "\n")
 
-    if student_answer == "q":
+    if inputs[i] == "q":
       break
 
     file.write(str(datetime.datetime.now()) + ": Local cache retrieval underway.\n")
     start_time3 = time.time()
     # Passing context the answer prompt expects
-    output = chain2.invoke({"models_answer": models_answers[i], "question": questions[i], "student_answer":student_answer})
+    output = chain2.invoke({"models_answer": models_answers[i], "question": questions[i], "student_answer":inputs[i]})
     end_time3 = time.time()
     file.write(str(datetime.datetime.now()) + ": Cached data retrieval successful\n")
     file.write(str(datetime.datetime.now()) + ": Model: " + output  + "\n")
     file.write("Cache retrieval latency: " + str(end_time3 - start_time3))
     print(output)
 
+    # If model accurately marked with the correct, expected answer
+    # proving the models knowledge
+    if expected_answers[i] in output:
+      model_acc_count+=1
+
     print("---------------------------")
+  
+  print("System: The quiz is finally over, now that took a long time didn't it!\n")
+  # Want this to be very close to 15/15
+  print(f"System: The models accuracy is {model_acc_count}/15\n")
+  file.write(str(datetime.datetime.now()) + ": The models accuracy is: " + str(model_acc_count) + "/15\n")
+
+  if model_acc_count >= 13:
+    print("System: The model was highly accurate, and capable of grading a student.\n")
+    file.write(str(datetime.datetime.now()) + ": The model was highly accurate.\n")
+  else:
+    print("System: The model is not accurate enough to successfully grade a student.\n")
+    file.write(str(datetime.datetime.now()) + ": The model is not able to accurately grade a student.\n")
+  
+  
